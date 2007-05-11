5f1e0f615e160eeba9e02a9f9b3bdb456c0f9d03
rodrigopex <rodrigopex@36eccd61-3230-0410-a8ad-a51b5c886b2f>
2007-05-11 16:19:42 +0000

    Unittests Refactoring.

--- trunk/src/alu_unit_test.py
+++ trunk/src/alu_unit_test.py
@@ -19,45 +19,77 @@ from alu import *
 
 class alu_unit_test( TestCase ):
 
-    def test_case_addition( self ):
-        """ Check addition between 32 bit integers"""
+    def abstract_test_case( self, selection , func ):
         def test( a, b, sel, result, error ):
             for i in range( 10 ):
                 r_a, r_b = randrange( 32 ), randrange( 32 )
                 a.next, b.next = r_a, r_b
-                sel.next = 0x0
+                sel.next = selection
                 yield delay( 10 )
-                expect = r_a + r_b
+                expect = func( r_a, r_b )
                 actual = result
-                self.assertEqual( actual, expect )
+                self.assertEqual( actual, intbv( expect )[32:] )
 
         for width in range( 100 ):
             a, b, result = [Signal( intbv( 0 )[32:] ) for i in range( 3 )]
-            sel = Signal(intbv(0)[4:])
-            error = Signal(intbv(0)[1:])
+            sel = Signal( intbv( 0 )[4:] )
+            error = Signal( intbv( 0 )[1:] )
             sum = ALU( a, b, sel, result, error )
-            check = test(a, b, sel, result, error)
+            check = test( a, b, sel, result, error )
             sim = Simulation( sum, check )
             sim.run( quiet=1 )
 
+    def test_case_addition( self ):
+        """ Check Addition between 32 bit vectors"""
+        return self.abstract_test_case( 0x0, lambda a, b: a+b )
+
     def test_case_subtraction( self ):
-        """ Check subtraction between 32 bit integers"""
+        """ Check Subtraction between 32 bit vectors"""
+        return self.abstract_test_case( 0x1, lambda a, b: a - b )
+
+    def test_case_and( self ):
+        """ Check AND bit between 32 bit vectors"""
+        return self.abstract_test_case(0x2, lambda a,b : a & b)
+
+    def test_case_or( self ):
+        """ Check OR bit between 32 bit vectors"""
+        return self.abstract_test_case(0x3, lambda a,b : a | b)
+
+    def test_case_not( self ):
+        """ Check NOT bit 32 bit vectors"""
+        return self.abstract_test_case(0x4, lambda a,b : ~ a )
+
+    def test_case_xor( self ):
+        """ Check XOR bit between 32 bit vectors"""
+        return self.abstract_test_case(0x5, lambda a,b : a ^ b )
+
+    def test_case_Multiplication( self ):
+        """ Check Multiplication between 32 bit vectors"""
+        return self.abstract_test_case(0x6, lambda a,b : a * b )
+
+    def test_case_Division( self ):
+        """ Check Division between 32 bit vectors"""
         def test( a, b, sel, result, error ):
             for i in range( 10 ):
                 r_a, r_b = randrange( 32 ), randrange( 32 )
+                #r_a, r_b = randrange( 32 ), intbv(0)[32:]
                 a.next, b.next = r_a, r_b
-                sel.next = 0x1
+                sel.next = 0x7
                 yield delay( 10 )
-                expect = r_a - r_b
-                actual = result
-                self.assertEqual( actual, intbv(expect)[32:] )
+                if r_b == intbv(0)[32:]:
+                    self.assertEquals( error , intbv(1)[1:] )
+                    self.assertEquals( result , intbv(0)[32:] )
+                else:
+                    expect = r_a // r_b
+                    actual = result
+                    self.assertEqual( actual, intbv(expect)[32:] )
 
         for width in range( 100 ):
             a, b, result = [Signal( intbv( 0 )[32:] ) for i in range( 3 )]
-            sel = Signal(intbv(0)[4:])
-            error = Signal(intbv(0)[1:])
+            sel = Signal( intbv( 0 )[4:] )
+            error = Signal( intbv( 0 )[1:] )
             sum = ALU( a, b, sel, result, error )
-            check = test(a, b, sel, result, error)
+            check = test( a, b, sel, result, error )
             sim = Simulation( sum, check )
             sim.run( quiet=1 )
 
